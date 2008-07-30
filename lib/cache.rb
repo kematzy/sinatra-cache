e196ad1b09668dd2a81266598750efe0500b1f8a
Kematzy <kematzy@gmail.com>
2008-07-31 00:32:15 +0100

    -- Added functioning logging -- Changed the default cache_dir -- Added TODOs comments -- Added options hash to methods -- Improved/added comments

--- lib/cache.rb
+++ lib/cache.rb
@@ -1,6 +1,6 @@
 
 require 'fileutils'
-# require 'logger'
+require 'logger'
 
 
 module Sinatra
@@ -10,8 +10,9 @@ module Sinatra
   module Cache
     
     
-    # TODO: make the logging stuff work better
+    # TODO: move the logging stuff to a better location. Where? Set the default level ?
     $LOG = Logger.new(STDERR)
+    # $LOG.level = Logger::INFO
     
     # TODO: move these config settings somewhere else, or improve upon them
     # toggle for cache functionality
@@ -19,8 +20,7 @@ module Sinatra
     # default extension for caching
     set :cache_page_extension, '.html'
     # set Cache dir to Root of Public.
-    set :cache_dir, 'system/cache/'
-    
+    set :cache_dir, ''  # set to empty, since the ideal 'system/cache/' does not work with Passenger & mod_rewrite :(
     
     # Caches the given URI to a html file in /public
     # 
@@ -28,11 +28,12 @@ module Sinatra
     #    >> cache( erb(:contact, :layout => :layout))
     #      =>  returns the HTML output written to /public/<CACHE_DIR_PATH>/contact.html
     #
+    #  TODO:: implement the options={} hash functionality. What options are really needed ? 
     def cache(content, options={})
       return content unless Sinatra.options.cache_enabled
       
       unless content.nil?
-        path = cache_page_path(request.path_info)
+        path = cache_page_path(request.path_info,options)
         FileUtils.makedirs(File.dirname(path))
         open(path, 'wb+') { |f| f << content }
         $LOG.info( "Cached Page: [#{path}]") 
@@ -50,7 +51,8 @@ module Sinatra
     #     expire_cache   # deletes the /public/<CACHE_DIR_PATH>contact.html page as well
     #    end
     #
-    def expire_cache(path = nil)
+    #  TODO:: implement the options={} hash functionality. What options are really needed ? 
+    def expire_cache(path = nil, options={})
       return unless Sinatra.options.cache_enabled
       
       path = (path.nil?) ? cache_page_path(request.path_info) : cache_page_path(path)
@@ -62,6 +64,7 @@ module Sinatra
       end
     end
     
+    
     # Prints a basic HTML comment with a timestamp in it.
     # 
     # *NB!* IE6 does NOT like this to be the first line of a HTML document, so output
@@ -72,19 +75,25 @@ module Sinatra
     #      => <!--  page cached: 2008-08-01 12:00:00 -->
     #
     def page_cached_timestamp
-      "<!--  page cached: #{Time.now.strftime("%Y-%d-%m %H:%M:%S")} -->" if Sinatra.options.cache_enabled
+      "<!-- page cached: #{Time.now.strftime("%Y-%d-%m %H:%M:%S")} -->" if Sinatra.options.cache_enabled
     end
+    
     
     private
       
-      def cache_file_name(path)
+      # establishes the file name of the cached file from the path given
+      #  TODO:: implement the options={} functionality. Not sure IF we really need the :extension option or not?? 
+      def cache_file_name(path,options={})
         name = (path.empty? || path == "/") ? "index" : Rack::Utils.unescape(path.sub(/^(\/)/,'').chomp('/'))
         name << Sinatra.options.cache_page_extension unless (name.split('/').last || name).include? '.'
         return name
       end
       
-      def cache_page_path(path)
-        "#{Sinatra.options.public}/#{Sinatra.options.cache_dir}#{cache_file_name(path)}"
+      # sets the full path to the cached page/file
+      # Dependent upon Sinatra.options .public and .cache_dir variables being present and set.
+      # 
+      def cache_page_path(path,options={})
+        "#{Sinatra.options.public}/#{Sinatra.options.cache_dir}#{cache_file_name(path,options)}"
       end
       
       
