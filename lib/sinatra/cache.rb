8a5414c90c6505941ae84b2f740d567d57897b46
kematzy <kematzy@gmail.com>
2009-02-25 00:55:09 +0800

    General code cleanup. Fixed a few issues and added comments

--- lib/sinatra/cache.rb
+++ lib/sinatra/cache.rb
@@ -5,9 +5,11 @@ module Sinatra
     
   # Sinatra Caching module
   # 
+  #  TODO:: Need to write documentation here 
+  # 
   module Cache
     
-    VERSION = 'Sinatra::Cache v0.0.2'
+    VERSION = 'Sinatra::Cache v0.2.0'
     def self.version; VERSION; end
     
     
@@ -21,18 +23,18 @@ module Sinatra
       # 
       # Also accepts an Options Hash, with the following options:
       #  * :extension => in case you need to change the file extension
-      #  * 
-      #  TODO:: implement the opts={} hash functionality. What options are needed?
+      #  
+      #  TODO:: implement the opts={} hash functionality. What other options are needed?
+      # 
       def cache(content, opts={})
         return content unless options.cache_enabled
-        # return content unless self.cache_enabled
         
         unless content.nil?
+          content = "#{page_cached_timestamp}#{content}\n"
           path = cache_page_path(request.path_info,opts)
-          log.info("path=[#{path}]")
-          # FileUtils.makedirs(File.dirname(path))
-          # open(path, 'wb+') { |f| f << content }
-          # log.info( "Cached Page: [#{path}]") 
+          FileUtils.makedirs(File.dirname(path))
+          open(path, 'wb+') { |f| f << content }
+          log("Cached Page: [#{path}]",:info) 
           content
         end
       end
@@ -54,79 +56,82 @@ module Sinatra
         path = (path.nil?) ? cache_page_path(request.path_info) : cache_page_path(path)
         if File.exist?(path)
           File.delete(path)
-          log.info( "Expired Page deleted at: [#{path}]")
+          log("Expired Page deleted at: [#{path}]",:info)
         else
-          log.info( "No Expired Page was found at the path: [#{path}]")
+          log("No Expired Page was found at the path: [#{path}]",:info)
         end
       end
       
-      
-      # Prints a basic HTML comment with a timestamp in it.
+      # Prints a basic HTML comment with a timestamp in it, so that you can see when a file was cached last.
       # 
       # *NB!* IE6 does NOT like this to be the first line of a HTML document, so output
       # inside the <head> tag. Many hours wasted on that lesson ;-)
       # 
       # <b>Usage:</b>
       #    >> <%= page_cached_timestamp %>
-      #      => <!--  page cached: 2008-08-01 12:00:00 -->
+      #      => <!--  page cached: 2009-02-24 12:00:00 -->
       #
       def page_cached_timestamp
-        "<!-- page cached: #{Time.now.strftime("%Y-%d-%m %H:%M:%S")} -->" if options.cache_enabled
+        "<!-- page cached: #{Time.now.strftime("%Y-%d-%m %H:%M:%S")} -->\n" if options.cache_enabled
       end
       
-      # 
-      # 
+      
       private
-      #   
-        # establishes the file name of the cached file from the path given
-        #  TODO:: implement the opts={} functionality. Not sure IF we really need the :extension option or not?? 
+        
+        # Establishes the file name of the cached file from the path given
+        # 
+        # TODO:: implement the opts={} functionality, and support for custom extensions on a per request basis. 
+        # 
         def cache_file_name(path,opts={})
           name = (path.empty? || path == "/") ? "index" : Rack::Utils.unescape(path.sub(/^(\/)/,'').chomp('/'))
           name << options.cache_page_extension unless (name.split('/').last || name).include? '.'
           return name
         end
-
-        # sets the full path to the cached page/file
+        
+        # Sets the full path to the cached page/file
         # Dependent upon Sinatra.options .public and .cache_dir variables being present and set.
         # 
         def cache_page_path(path,opts={})
-          # test if given a full path rather than relative path, otherwise join the public path to cache_dir
-          cache_dir = (options.cache_dir == File.expand_path(options.cache_dir)) ? options.cache_dir : "#{options.public}/#{options.cache_dir}"
+          # test if given a full path rather than relative path, otherwise join the public path to cache_dir 
+          # and ensure it is a full path
+          cache_dir = (options.cache_dir == File.expand_path(options.cache_dir)) ? 
+              options.cache_dir : File.expand_path("#{options.public}/#{options.cache_dir}")
           cache_dir = cache_dir[0..-2] if cache_dir[-1,1] == '/'
           "#{cache_dir}/#{cache_file_name(path,opts)}"
         end
         
-        
-        #  TODO:: need to make this work with the actual settings.... 
+        #  TODO:: this implementation really stinks, how do I incorporate Sinatra's logger??
         def log(msg,scope=:debug)
-          require 'logger'
-          logger ||= Logger.new(STDERR)
-          logger.level = Logger::DEBUG
-          # $LOG.level = Logger::INFO
-          logger.debug(msg)
+          if options.cache_logging
+            "Log: msg=[#{msg}]" if scope == options.cache_logging_level
+          else
+            # just ignore the stuff...
+            # puts "just ignoring msg=[#{msg}] since cache_logging => [#{options.cache_logging.to_s}]"
+          end
         end
-      
+        
     end #/module Helpers
     
     
+    # Sets the default options:
     # 
+    #  * +:cache_enabled+ => toggle for the cache functionality. Default is: +true+
+    #  * +:cache_page_extension+ => sets the default extension for cached files. Default is: +.html+
+    #  * +:cache_dir+ => sets cache directory where cached files are stored. Default is: ''(empty) == root of /public.<br>
+    #      set to empty, since the ideal 'system/cache/' does not work with Passenger & mod_rewrite :(
+    #  * +cache_logging+ => toggle for logging the cache calls. Default is: +true+
+    #  * +cache_logging_level+ => sets the level of the cache logger. Default is: <tt>:info</tt>.<br>
+    #      Options:(unused atm) [:info, :warn, :debug]
     # 
     def self.registered(app)
       app.helpers(Cache::Helpers)
-      
-      # toggle for cache functionality
       app.set :cache_enabled, true
-      # default extension for cached files
       app.set :cache_page_extension, '.html'
-      # set Cache dir to Root of Public.
-      app.set :cache_dir, ''  # set to empty, since the ideal 'system/cache/' does not work with Passenger & mod_rewrite :(
-      # toggle for logging cache calls
+      app.set :cache_dir, ''
       app.set :cache_logging, true
-      # set the level of the cache logger. Default is :debug. Options: [:info, :warn, :debug]
-      app.set :cache_logging_level, :debug
+      app.set :cache_logging_level, :info
     end
     
-          
   end #/module Cache
   
   register(Sinatra::Cache)
