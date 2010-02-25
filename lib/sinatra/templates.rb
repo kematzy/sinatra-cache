99262883b90083513134596736dc393db0cc3990
kematzy <kematzy@gmail.com>
2010-02-25 12:11:35 +0800

    Reworked Sinatra::Template#render monkey-patch

--- lib/sinatra/templates.rb
+++ lib/sinatra/templates.rb
@@ -1,7 +1,13 @@
 
 module Sinatra 
   
-  
+  ##
+  # Sinatra::Templates
+  # 
+  # Monkey-patches the private <tt>#render</tt> method, 
+  # in order to support auto-magic cache functionality.
+  # 
+  # 
   module Templates 
     
     private
@@ -12,41 +18,72 @@ module Sinatra
       # ==== Examples
       # 
       # 
-      def render(engine, template, options={}, locals={})
+      def render(engine, data, options={}, locals={}, &block) 
         # merge app-level options
-        options = self.class.send(engine).merge(options) if self.class.respond_to?(engine)
+        options = settings.send(engine).merge(options) if settings.respond_to?(engine)
         
         # extract generic options
+        locals = options.delete(:locals) || locals || {}
+        views = options.delete(:views) || settings.views || "./views"
         layout = options.delete(:layout)
         layout = :layout if layout.nil? || layout == true
-        views = options.delete(:views) || self.class.views || "./views"
-        locals = options.delete(:locals) || locals || {}
         
         # set the cache related options
-        cache_enabled = self.class.send(:cache_enabled)  if self.class.respond_to?(:cache_enabled)
-        cache_enabled = false unless cache_enabled === true
-        cache_output_dir = self.class.send(:cache_output_dir)  if self.class.respond_to?(:cache_output_dir)
+        cache_enabled = settings.respond_to?(:cache_enabled) ? settings.send(:cache_enabled) : false
+        # cache_enabled = settings.send(:cache_enabled) if settings.respond_to?(:cache_enabled)
+        # cache_enabled = false unless cache_enabled === true
+        cache_output_dir = settings.send(:cache_output_dir)  if settings.respond_to?(:cache_output_dir)
         # raise Exception, "The Sinatra::Cache cache_output_dir variable is pointing to a non-existant directory cache_output_dir=[#{cache_output_dir}]" unless test(?d, cache_output_dir)
         cache_option = options.delete(:cache)
         cache_option = true if cache_option.nil?
         
-        # render template
-        data, options[:filename], options[:line] = lookup_template(engine, template, views)
-        output = __send__("render_#{engine}", data, options, locals)
+        # compile and render template
+        template = compile_template(engine, data, options, views)
+        output = template.render(self, locals, &block)
         
         # render layout
-        if layout 
-          data, options[:filename], options[:line] = lookup_layout(engine, layout, views)
-          if data
-            output = __send__("render_#{engine}", data, options, locals) { output }
+        if layout
+          begin
+            options = options.merge(:views => views, :layout => false)
+            output = render(engine, layout, options, locals) { output }
+            # Cache the content or just return it
+            (cache_enabled && settings.send(:environment) == settings.cache_environment) ? 
+                cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"")) : output
+          rescue Errno::ENOENT
           end
-          (cache_enabled && self.class.send(:environment) == self.class.cache_environment) ? 
-              cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"")) : output
-        else
-          # rendering without a layout
-          (cache_enabled && cache_option && self.class.send(:environment) == self.class.cache_environment) ? 
-              cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"") ) : output
         end
+        
+        # rendering without a layout
+        (cache_enabled && cache_option && settings.send(:environment) == settings.cache_environment) ? 
+            cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"") ) : output
+        
+        
+        
+        # if layout 
+        #   data, options[:filename], options[:line] = lookup_layout(engine, layout, views)
+        #   if data
+        #     output = __send__("render_#{engine}", data, options, locals) { output }
+        #   end
+        #   (cache_enabled && settings.send(:environment) == settings.cache_environment) ? 
+        #       cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"")) : output
+        # else
+        #   # rendering without a layout
+        #   (cache_enabled && cache_option && settings.send(:environment) == settings.cache_environment) ? 
+        #       cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"") ) : output
+        # end
+        # # render layout
+        # if layout 
+        #   data, options[:filename], options[:line] = lookup_layout(engine, layout, views)
+        #   if data
+        #     output = __send__("render_#{engine}", data, options, locals) { output }
+        #   end
+        #   (cache_enabled && self.class.send(:environment) == self.class.cache_environment) ? 
+        #       cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"")) : output
+        # else
+        #   # rendering without a layout
+        #   (cache_enabled && cache_option && self.class.send(:environment) == self.class.cache_environment) ? 
+        #       cache_write_file(cache_file_path, output.gsub(/\n\r?$/,"") ) : output
+        # end
       end
       
   end #/module Templates
