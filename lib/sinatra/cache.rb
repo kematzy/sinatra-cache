ac60e9a4932d8049e0f1e7f2d5d4db285590a41e
kematzy <kematzy@gmail.com>
2010-05-14 12:36:06 +0800

    Version bump to 0.3.3

--- lib/sinatra/cache.rb
+++ lib/sinatra/cache.rb
@@ -4,7 +4,7 @@ require 'sinatra/outputbuffer'
 
 module Sinatra
   module Cache
-    VERSION = '0.3.2' unless const_defined?(:VERSION)
+    VERSION = '0.3.3' unless const_defined?(:VERSION)
     def self.version; "Sinatra::Cache v#{VERSION}"; end
   end #/ Cache
 end #/ Sinatra
